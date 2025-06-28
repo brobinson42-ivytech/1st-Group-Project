559d256d12118296441e505067fd657fb31c547b
brobinson42-ivytech <brobinson42@ivytech.edu>
2025-06-28 12:23:59 -0400

    Add files via upload

---
+++ pay calculator.py
@@ -0,0 +1,11 @@
+hours_worked = float(input("How many hours did you work this week?"))
+if hours_worked <= 40:
+ gross = hours_worked * 29.28
+ print(f"You made ${gross:.2f} this week")
+else:
+    gross = 40 * 29.28
+    over = hours_worked - 40
+    overtime = over * (29.28 * 1.5)
+    total = gross + overtime
+    print(f"You made ${total:.2f} this week")
+input("Press enter to continue")
