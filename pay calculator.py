b1916dc9616ffad8ebab0a8311a6731c49f8ac26
brobinson42-ivytech <brobinson42@ivytech.edu>
2025-06-30 17:59:13 -0400

    Update pay calculator.py
    Added code if letters are entered instead of numbers

--- pay calculator.py
+++ pay calculator.py
@@ -1,9 +1,12 @@
 while True:
- hours_worked = float(input("How many hours did you work this week?"))
- if hours_worked < 0:
-  print("Hours worked cannot be negative. Please enter a valid number.")
- else:
-  break
+ try:
+  hours_worked = float(input("How many hours did you work this week?"))
+  if hours_worked < 0:
+   print("Hours worked cannot be negative. Please enter a valid number.")
+  else:
+   break
+ except: ValueError
+ print("Why you putting letters? You crazy. Numbers only, son")
 if hours_worked <= 40:
  gross = hours_worked * 29.28
  print(f"You made ${gross:.2f} this week")
